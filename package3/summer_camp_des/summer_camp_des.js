17828b3524a0eba44dea524351d744de09a72246
wenhan <1196086345@qq.com>
2021-02-04 13:06:20 +0800

    页面全部完成

---
+++ package3/summer_camp_des/summer_camp_des.js
@@ -0,0 +1,74 @@
+// package3/summer_camp_des/summer_camp_des.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    camp:{
+      title:'2020拼搏体育14天足球夏令营',
+      price:5000,
+      people:'6~15周岁',
+      address:'上海',
+      author:'兴致体育',
+      duration:'7天',
+      content:'<div>Hello World!</div>'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
