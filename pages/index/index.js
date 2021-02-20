58febd3ac0a15aef5ebebd0fd9ace1c23f1ef4cd
wenhan <1196086345@qq.com>
2021-02-21 01:50:52 +0800

    增加免费体验课程

--- pages/index/index.js
+++ pages/index/index.js
@@ -131,7 +131,8 @@ Page({
         console.log(res.data)
         // wx.hideLoading()
         that.setData({
-          favor_course: res.data,
+          favor_course: res.data[0],
+          free_course:res.data[1]
         })
       }
     })
